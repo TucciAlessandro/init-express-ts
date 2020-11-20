8f6b986250712c1cfc913470b352044f16aebed5
Unhookedx <tuccialessandrox@gmail.com>
2020-11-20 15:46:47 +0100

    backend

--- src/app.ts
+++ src/app.ts
@@ -2,15 +2,52 @@ import express, { Application } from "express";
 
 const app: Application = express();
 
-const port = 5000;
+const port = 6000;
 
-const exampleJson = {
-  value: "test",
-  arrayOfValues: ["one", "two", "three"]
+interface Restaurant {
+  name: string;
+  id: number;
+  typeOfKitchen: string;
+  description: string;
+  rating: number;
+}
+
+const Restaurants: Restaurant[] = [
+  {
+    name: "Ristorante Alessandro",
+    id: 2,
+    typeOfKitchen: "Italiana",
+    description: "oijhef0wjfw-ofkwfk",
+    rating: 4.5 / 5,
+  },
+  {
+    name: "Ristorante Prova",
+    id: 2,
+    typeOfKitchen: "Italiana",
+    description: "oijhef0wjfw-ofkwfk",
+    rating: 4.5 / 5,
+  },
+  {
+    name: "Ristorante Mattia",
+    id: 2,
+    typeOfKitchen: "Italiana",
+    description: "oijhef0wjfw-ofkwfk",
+    rating: 4.5 / 5,
+  },
+];
+
+const filterList = (param: string): Restaurant[] => {
+  const filteredList = Restaurants.filter((restaurant) =>
+    restaurant.name.toLowerCase().includes(param)
+  );
+  return filteredList;
 };
 
-app.get("/", (req, res) => {
-  res.send(exampleJson);
+app.get("/restaurants", (req, res) => {
+  const query = req.query.name;
+  const filteredList = filterList(query);
+  console.log(filteredList);
+  res.send(filteredList);
 });
 
 app.listen(port, () => console.log(`listening on port: ${port}`));
